0f9aad550e0fcd94776ada0df0558a432e0b7c37
cherish <lcl1380@naver.com>
2024-02-28 09:29:41 +0900

    컨테이너 켜기 & ssh 열기 자동화 스크립트 병합 완료

--- docker_restart.sh
+++ docker_restart.sh
@@ -7,3 +7,8 @@ docker restart log_collection
 docker restart worker1
 docker restart worker2
 docker restart worker3
+
+# nginx-proxy 컨테이너에서 SSH 서비스 재시작
+docker exec -it nginx-proxy bash -c "service ssh restart"'
+
+echo "모든 컨테이너에서 restart 완료, SSH 서비스가 재시작되었습니다."
